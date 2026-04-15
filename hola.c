648f0cfcd352b1ca809a8917b6d28a226e93a32a
Jjonathanpp <jonathan1.perez750@gmail.com>
2026-04-15 11:40:49 -0300

    el hola.c

--- hola.c
+++ hola.c
@@ -2,10 +2,20 @@
  * Imprime un saludo por la salida estándar.
  */
 
-#include <stdlib.h>     // exit()
+#include <stdlib.h> // exit()
+#include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    // Termina la ejecución del proceso.
+    char Nombre[50];
+
+    if (argc < 2)
+    {
+        printf("Uso: %s <nombre>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Hola %s\n", argv[1]);
+
     exit(EXIT_SUCCESS);
 }
